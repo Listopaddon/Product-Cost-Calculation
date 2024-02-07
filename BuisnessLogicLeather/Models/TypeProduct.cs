5f605c398492543814bcec91622415637f5c8de1
Listopaddon <67251998+Listopaddon@users.noreply.github.com>
2024-02-07 11:51:50 +0300

    Add project files.

---
+++ BuisnessLogicLeather/Models/TypeProduct.cs
@@ -0,0 +1,13 @@
+﻿namespace BuisnessLogicLeather.Models
+{
+    public enum TypeProduct
+    {
+        Wallet,
+        MenBag,
+        LadyBag,
+        Backpack,
+        Longer,
+        BeltsAndWatchBands,
+        Other
+    }
+}
